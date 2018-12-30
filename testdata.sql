180b09e2463b61cc4169034c9c3c4f3025464f58
Marc Bertens - Administrator <m.bertens@pe2mbs.nl>
2018-12-30 10:12:36 +0100

    remove the testdata.sql file.

--- testdata.sql
+++ testdata.sql
@@ -9,3 +9,7 @@ insert into WA_USERS ( D_USER_NAME, D_PASSWORD, D_FIRST_NAME, D_MIDDLE_NAME, D_L
        values ( 'mbertens', '5701mb', 'Marc', '', 'Bertens-Nguyen', 1 );
 insert into WA_USERS ( D_USER_NAME, D_PASSWORD, D_FIRST_NAME, D_MIDDLE_NAME, D_LAST_NAME, D_ROLE_ID )
        values ( 'erijerse', '123456', 'Ernst', '', 'Rijerse', 1 );
+
+
+
+
