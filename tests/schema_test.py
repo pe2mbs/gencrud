789fd409cff95b9be2793da529293c6c00bcd07c
giorgi <giorgilomidze97@googlemail.com>
2022-04-22 17:26:22 +0200

    introduced pytest and flake8 linting

--- tests/schema_test.py
+++ tests/schema_test.py
@@ -2,16 +2,14 @@ import yaml
 import json
 import jsonschema
 from gencrud.schema import GENCRUD_SCHEME
-
+import os
 
 
 def test():
-    filename = r'C:\src\python\testrun-web\template\test_message\te_format.yaml'
+    filename = os.path.join(os.getcwd(), 'tests', 'input', 'te_format.yaml')
 
     with open( filename, 'r' ) as stream:
         data = yaml.load( stream, Loader=yaml.Loader )
-
-    print( json.dumps( data, indent= 4 ) )
 
     try:
         jsonschema.Draft7Validator( GENCRUD_SCHEME )
@@ -25,8 +23,4 @@ def test():
         print( exc )
         raise SystemExit
 
-
-    return
-
-if __name__ == '__main__':
-    test()
+    assert True
