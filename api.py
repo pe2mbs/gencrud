d3428f0b1d11821141bf6eca44dacd4f6b79106a
Marc Bertens-Nguyen <marc.bertens@worldline.com>
2022-05-30 09:18:31 +0200

    Temp. fix

--- api.py
+++ api.py
@@ -18,6 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 import os
+from tasks.tsk_common.constants import *
 from flask_marshmallow import Marshmallow
 
 app             = None
